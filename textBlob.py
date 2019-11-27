4c280f4a2dad75186977ab68059a07ba500d3346
Kumari-Manisha <manishakarn@gmail.com>
2019-11-27 11:43:07 -0500

    changed

--- textBlob.py
+++ textBlob.py
@@ -37,7 +37,7 @@ def textBlobGo(file):
     for i in range(len(data)):
         data.loc[i,'text']=data.loc[i,'text'].replace("’","'")
 
-    sys.path.insert(0, 'C:\\Users\\gautam\\Desktop\\Tweets_Notebook')
+    #sys.path.insert(0, 'path_contractions_1')
     from contractions_1 import CONTRACTION_MAP
 
     def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
